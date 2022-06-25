a757619825e3ed6a9183a134c10419e8276ff44b
«Aleksandr» <navalniu202@gmail.com>
2022-06-25 17:44:54 +0300

    HomeWork001

---
+++ HomeWork_001/Program.cs
@@ -0,0 +1,16 @@
+﻿int SecondNumber()
+{
+          Console.WriteLine("Введите трехзначное число");
+          
+          int number = Convert.ToInt32(Console.ReadLine());
+
+          int dec = number / 10;
+
+          int ed = dec % 10;
+
+          return ed;
+}
+
+int secnumber = SecondNumber();
+
+Console.WriteLine("Вторая цифра этого числа = " + secnumber);
