4c6978c23f70966dc810844025b8a13931c2004d
«Aleksandr» <navalniu202@gmail.com>
2022-06-26 17:17:57 +0300

    homework002

---
+++ HomeWork_002/Program.cs
@@ -0,0 +1,33 @@
+﻿int ThreeNumber();
+{
+   Console.WriteLine("введите число");
+
+   int num = Convert.ToInt32(Console.ReadLine());
+
+   if (num < 100)
+   {
+      Console.Write("Третьей цифры нет");
+   }
+   else
+   {
+   while (num >= 1000)
+   {
+      num = num / 10;
+      return num;
+   }
+
+   if (num < 1000)
+   { 
+
+      int three = num % 100;
+      int too = three % 10;
+      Console.Write(too);
+      return too;
+   
+   }  
+}
+}
+
+int trenum = ThreeNumber();
+
+Console.WriteLine(trenum);
